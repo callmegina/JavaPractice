5246b1af0b8f3f9a56067c7aa6328cab2e3965c3
callmegina <ginnageoxy@gmail.com>
2022-12-27 23:45:37 +1100

    modified

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -1,21 +1,42 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReverseArray {
 	
+	
+	//Approach 1
 	public static int[] reverseTest(int[] test) {
-		
+
 		int newArray[] = new int[test.length];
-		
-		for(int i = 0; i< test.length; i++) {
-			
+
+		for (int i = 0; i < test.length; i++) {
+			newArray[test.length - i - 1] = test[i];
+			//System.out.println("new array" + Arrays.toString(newArray));
+			//System.out.println("old array" + Arrays.toString(test));
 		}
-		return null;
-				
+		return newArray;
+	}
+	
+	
+	//Approach 2
+	
+	public static void reverse(Integer[] a) {
+		
+		Collections.reverse(Arrays.asList(a));
+		System.out.println(Arrays.asList(a));
+		
 	}
 
 	public static void main(String[] args) {
 		
-
+		Integer []test = {33,3,45,1}; 
+		int[] result; 
+		reverse(test);
+		//System.out.println(Arrays.toString(result));
+	
 	}
 
 }
